ba90ed9a43698ca11b097e94075c961e2212ab7d
Unknown <amanda.ks.poh@gmail.com>
2018-04-05 11:38:13 -0700

    Pre-About Page Changes

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+// Show and hide image on hover
+
+document.getElementById("caption").style.display = "block";
+
+function showBlock(id) {
+  document.getElementById(id).style.visibility = "visible";
+};
+function hideBlock(id) {
+  document.getElementById(id).style.visibility = "hidden";
+};
