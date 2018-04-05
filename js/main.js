30a3462dcdd8c1bceb2f4dbb85209421f8808c9c
Unknown <amanda.ks.poh@gmail.com>
2018-04-05 14:26:33 -0700

    Desktop mouse-hover working

--- js/main.js
+++ js/main.js
@@ -1,13 +1,60 @@
 "use strict";
 
+console.log("Script is running!");
+
+var portraitLink = document.querySelector("#portrait-link");
+var portrait = document.querySelector("#portrait");
+
+var oneasicsLink = document.querySelector("#oneasics-link");
+var oneasics = document.querySelector("#oneasics");
+
+var inamotoLink = document.querySelector("#inamoto-link");
+var inamoto = document.querySelector("#inamoto");
+
+var hootsuiteLink = document.querySelector("#hootsuite-link");
+var hootsuite = document.querySelector("#hootsuite");
+
+var akqaLink = document.querySelector("#akqa-link");
+var akqa = document.querySelector("#akqa");
 
 // Show and hide image on hover
 
-document.getElementById("caption").style.display = "block";
+portraitLink.onmouseenter = function(){
+  portrait.classList.remove("hidden");
+};
 
-function showBlock(id) {
-  document.getElementById(id).style.visibility = "visible";
+// portraitLink.onmouseleave = function(){
+//   portrait.classList.add("hidden");
+// };
+
+oneasicsLink.onmouseenter = function(){
+  oneasics.classList.remove("hidden");
 };
-function hideBlock(id) {
-  document.getElementById(id).style.visibility = "hidden";
+
+// oneasicsLink.onmouseleave = function(){
+//   oneasics.classList.add("hidden");
+// };
+
+inamotoLink.onmouseenter = function(){
+  inamoto.classList.remove("hidden");
 };
+
+// inamotoLink.onmouseleave = function(){
+//   inamoto.classList.add("hidden");
+// };
+
+hootsuiteLink.onmouseenter = function(){
+  hootsuite.classList.remove("hidden");
+};
+
+// hootsuiteLink.onmouseleave = function(){
+//   hootsuite.classList.add("hidden");
+// };
+
+akqaLink.onmouseenter = function(){
+  akqa.classList.remove("hidden");
+};
+
+// akqaLink.onmouseleave = function(){
+//   akqa.classList.add("hidden");
+// };
