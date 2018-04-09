c8dc2cbf3cece569c50bcb0fddacd2f2be183a58
Unknown <amanda.ks.poh@gmail.com>
2018-04-08 23:47:34 -0700

    Media Queries for Hover working

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 "use strict";
 
+// Testing to make sure JS is running
 console.log("Script is running!");
 
-var portraitLink = document.querySelector("#portrait-link");
-var portrait = document.querySelector("#portrait");
+// Allowing JS to read the size of the window as a variable
+var windowWidth = window.innerWidth;
+console.log("Window width: "+windowWidth);
 
+// Allowing JS to read following ID's
 var oneasicsLink = document.querySelector("#oneasics-link");
 var oneasics = document.querySelector("#oneasics");
 
@@ -17,44 +20,42 @@ var hootsuite = document.querySelector("#hootsuite");
 var akqaLink = document.querySelector("#akqa-link");
 var akqa = document.querySelector("#akqa");
 
-// Show and hide image on hover
 
-portraitLink.onmouseenter = function(){
-  portrait.classList.remove("hidden");
-};
+// Media Query
+if (windowWidth > 1250) {
+  console.log("windowWidth > 1250px; Hover styling turned on.");
 
-// portraitLink.onmouseleave = function(){
-//   portrait.classList.add("hidden");
-// };
+  // Show and hide image on hover
+  oneasicsLink.onmouseenter = function(){
+    oneasics.classList.remove("hidden");
+  };
+  oneasicsLink.onmouseleave = function(){
+    oneasics.classList.add("hidden");
+  };
 
-oneasicsLink.onmouseenter = function(){
-  oneasics.classList.remove("hidden");
-};
+  inamotoLink.onmouseenter = function(){
+    inamoto.classList.remove("hidden");
+  };
+  inamotoLink.onmouseleave = function(){
+    inamoto.classList.add("hidden");
+  };
 
-// oneasicsLink.onmouseleave = function(){
-//   oneasics.classList.add("hidden");
-// };
+  hootsuiteLink.onmouseenter = function(){
+    hootsuite.classList.remove("hidden");
+  };
+  hootsuiteLink.onmouseleave = function(){
+    hootsuite.classList.add("hidden");
+  };
 
-inamotoLink.onmouseenter = function(){
-  inamoto.classList.remove("hidden");
-};
-
-// inamotoLink.onmouseleave = function(){
-//   inamoto.classList.add("hidden");
-// };
-
-hootsuiteLink.onmouseenter = function(){
-  hootsuite.classList.remove("hidden");
-};
-
-// hootsuiteLink.onmouseleave = function(){
-//   hootsuite.classList.add("hidden");
-// };
-
-akqaLink.onmouseenter = function(){
-  akqa.classList.remove("hidden");
-};
-
-// akqaLink.onmouseleave = function(){
-//   akqa.classList.add("hidden");
-// };
+  akqaLink.onmouseenter = function(){
+    akqa.classList.remove("hidden");
+  };
+  akqaLink.onmouseleave = function(){
+    akqa.classList.add("hidden");
+  };
+  
+}
+// Media Query – If the screen is smaller than 1250, don't display images on hover.
+else {
+    console.log("Hover styling turned off.");
+}
